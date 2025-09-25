5c976b5b98f02069a707fbd5d9e85603c5b0598e
janre99 <jan.repel99@gmail.com>
2025-09-25 15:29:33 +0200

    Optimalization & BEM & DOM

--- main.js
+++ main.js
@@ -6,3 +6,8 @@ console.log(`Mam na imie ${firstName} i mam ${age} lat`)
 const header = document.querySelector('.page-header__heading--js')
 
 header.innerHTML = `Mam na imie ${firstName} i mam ${age} lat`
+console.log(header)
+
+const contentJs = document.querySelector('.my-article_content--js')
+
+contentJs.innerHTML = 'jestem javascriptem'
